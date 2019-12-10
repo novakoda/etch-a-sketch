4862afbc543f7e7314200f5f1b4cc229078f4379
novakoda <novakoda9@gmail.com>
2019-12-09 20:48:22 -0600

    Add buttons to change colors

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const gridCont = document.getElementById('gridCont');
 const newGridBtn = document.getElementById('newGridBtn');
+
+const randomBtn = document.getElementById('randomBtn');
+const redBtn = document.getElementById('redBtn');
+const blueBtn = document.getElementById('blueBtn');
+const yellowBtn = document.getElementById('yellowBtn');
+const greenBtn = document.getElementById('greenBtn');
+const purpleBtn = document.getElementById('purpleBtn');
+const blackBtn = document.getElementById('blackBtn');
+
 let aGrid = false;
+let mouseDown = false;
+let color = '#000';
 
 function makeRows(sideLength) {
   deleteGrid();
@@ -12,13 +23,20 @@ function makeRows(sideLength) {
     cell.style.height = `${gridArea / sideLength}px`;
     cell.style.width = `${gridArea / sideLength}px`;
     cell.onmouseover = function(){
-      cell.style.backgroundColor = 'black';
+      cell.style.backgroundColor = color;
     };
     gridCont.appendChild(cell).className = "gridCell";
   };
 };
 
-
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  };
+  return color;
+};
 
 function clearGrid() {
   for(i = 0; i < cells.length; i++) {
@@ -44,6 +62,42 @@ newGridBtn.addEventListener("click", function() {
     aGrid = true;
   }
 );
+
+randomBtn.addEventListener("click", function() {
+    color = getRandomColor();
+  }
+);
+
+redBtn.addEventListener("click", function() {
+    color = 'red';
+  }
+);
+
+blueBtn.addEventListener("click", function() {
+    color = 'blue';
+  }
+);
+
+yellowBtn.addEventListener("click", function() {
+    color = 'yellow';
+  }
+);
+
+greenBtn.addEventListener("click", function() {
+    color = 'green';
+  }
+);
+
+purpleBtn.addEventListener("click", function() {
+    color = 'purple';
+  }
+);
+
+blackBtn.addEventListener("click", function() {
+    color = 'black';
+  }
+);
+
 
 // also tried this but it didn't work
 // document.getElementById("newGridBtn").onclick = clearGrid;
